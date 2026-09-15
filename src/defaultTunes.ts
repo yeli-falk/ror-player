7675a058fe4af5e3565aa8eacad54f3cee886e15
yeli-falk <92771409+yeli-falk@users.noreply.github.com>
2026-09-15 13:48:31 +0200

    Add 'Underground' category with patterns and example song

--- src/defaultTunes.ts
+++ src/defaultTunes.ts
@@ -2992,6 +2992,44 @@ const rawTunes: {[tuneName: string]: RawTune} = {
 			"Tune", "Tune", "Tune", "Tune"
 		]
 	},
+	'Underground': {
+		categories: [ "uncommon"],
+		sheet: sheetUrl + "underground.pdf",
+		descriptionFilename: "underground",
+		patterns: {
+			Tune: {
+				loop: true,
+				ls: 'X     X X     X X     X     X   ',
+				ms: 'X     X X     X X     X     X   ',
+				hs: 'X     X X     X X     X     X   ',
+				re: '          sfX     sfX     fX  fX',
+				sn: '....X.....X.X.......X.......X...',
+				ta: '  X X     X X     X X     X   X ',
+				ag: 'a  ao o a  o  o a  ao o         ',
+				sh: '................................'
+			},
+			'Call Break': {
+				ls: '            A               A                              A   ',
+				ms: '@ls',
+				hs: '@ls',
+				sn: '@ls',
+				ta: '@ls',
+				ag: '@ls',
+				sh: '@ls',
+				re: 's..X  X     A   s..X  X     A   s..X    S..X   S..X  X     A   ',
+			},
+			'Break 1': {
+				ls: 'X  XX X X  X  X X  XX X A       ',
+				ms: '@ls',
+				hs: '@ls',
+				re: '@ls',
+				sn: '@ls',
+				ta: '@ls',
+				ag: '@ls',
+				sh: '@ls'
+			},
+		exampleSong: [ "Call Break", "Tune", "Break 1", "Tune"]
+	},
 	'Van Harte Pardon': {
 		categories: [ "uncommon", "tricky" ],
 		sheet: sheetUrl + "van-harte-pardon.pdf",
